8848cd9e5443f2f3b710f6c4357eee2d8d3f8226
TSdebug <taworkspace01@gmail.com>
2025-09-11 03:20:05 +0530

    Corrected TextChange component

--- src/Components/TextChange.jsx
+++ src/Components/TextChange.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 const TextChange = () => {
-  const texts = ["Hi, I'm Aditi", "Hi, I'm Aditi", "Hi, I'm Aditi"];
+  const texts = ["Hi, I'm Tanushree", "Hi, I'm Tanushree", "Hi, I'm Tanushree"];
   const [currenText, setCurrentText] = useState("");
   const [endValue, setendValue] = useState(true);
   const [isForward, setIsForward] = useState(true);
