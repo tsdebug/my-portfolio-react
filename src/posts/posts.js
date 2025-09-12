585ad87b2da5e407c286eea38a4680e58b1a0337
TSdebug <taworkspace01@gmail.com>
2025-09-13 02:59:13 +0530

    Added the post contents

---
+++ src/posts/posts.js
@@ -0,0 +1,16 @@
+const posts = [
+  {
+    id: "2025-02-06-setup-jekyll-blog",
+    title: "Setup Jekyll Blog",
+    date: "2025-02-06",
+    file: "2025-02-06-setup-jekyll-blog.md"
+  },
+  {
+    id: "2025-02-10-another-blog",
+    title: "Another Blog Post",
+    date: "2025-02-10",
+    file: "2025-02-10-another-blog.md"
+  }
+];
+
+export default posts;
