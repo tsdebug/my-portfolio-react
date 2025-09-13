7a17b828e9d06eb4fa9fd283e25e28b8bd49d7ed
TSdebug <taworkspace01@gmail.com>
2025-09-14 01:54:10 +0530

    Corrected

--- src/posts/posts.js
+++ src/posts/posts.js
@@ -6,10 +6,22 @@ const posts = [
     file: "2025-02-06-setup-jekyll-blog.md"
   },
   {
-    id: "2025-02-10-another-blog",
-    title: "Another Blog Post",
-    date: "2025-02-10",
-    file: "2025-02-10-another-blog.md"
+    id: "2025-02-13-internship-journey",
+    title: "Intership Journey",
+    date: "2025-02-13",
+    file: "2025-02-13-internship-journey.md"
+  },
+  {
+    id: "2025-03-02-full-stack-development",
+    title: "Full Stack Development",
+    date: "2025-03-02",
+    file: "2025-03-02-full-stack-development.md"
+  },
+  {
+    id: "2025-04-27-document-object-model",
+    title: "Document Object Model",
+    date: "2025-04-27",
+    file: "2025-04-27-document-object-model.md"
   }
 ];
 
