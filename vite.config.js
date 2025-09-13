2fa27ced48f04fd62111a8d9c73b277d2cd7be68
TSdebug <taworkspace01@gmail.com>
2025-09-14 01:56:24 +0530

    Added @tailwindcss/typography

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import typography from '@tailwindcss/typography';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-})
+  plugins: [
+    tailwindcss(),
+    react(),
+    typography()
+  ],
+});
