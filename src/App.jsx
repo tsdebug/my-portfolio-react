b880b4890c9371e0a62d2503051eb72c7566985d
TSdebug <taworkspace01@gmail.com>
2025-09-16 04:12:20 +0530

    Final Blog implement part added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,42 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Experience from "./Components/Experience/Experience";
 import Footer from "./Components/Footer/Footer";
 import Home from "./Components/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
 import Projects from "./Components/Projects/Projects";
-import About from "./Components/About/About"
+import About from "./Components/About/About";
+
+import BlogList from "./Components/BlogList";
+import Post from "./Components/Post";
+
 function App() {
-return (
-<div className="bg-[#171d32] h-auto w-full overflow-hidden">
-  <Navbar />
-  <Home />
-  <About />
-  <Experience />
-  <Projects />
-  <Footer />
-</div>
-);
+  return (
+    <Router>
+      <div className="bg-[#171d32] h-auto w-full overflow-hidden">
+        <Navbar />
+        <Routes>
+          {/* Portfolio pages */}
+          <Route
+            path="/"
+            element={
+              <>
+                <Home />
+                <About />
+                <Experience />
+                <Projects />
+              </>
+            }
+          />
+
+          {/* Blog Routes */}
+          <Route path="/blog" element={<BlogList />} />
+          <Route path="/blog/:slug" element={<Post />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
+
 export default App;
