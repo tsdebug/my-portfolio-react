e11066d2a1b58fd5189712d5df3665588e56ce5e
TSdebug <taworkspace01@gmail.com>
2025-09-16 04:16:39 +0530

    Corrected style and now demo and source code works

--- src/Components/Projects/ProjectCard.jsx
+++ src/Components/Projects/ProjectCard.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import bannerImg from "../../assets/C.webp";
-const ProjectCard = ({ title, main }) => {
+
+const ProjectCard = ({ title, main, demoLink, sourceLink }) => {
   return (
     <div className="p-1 md:p-4 flex flex-col w-75 bg-[#0c0e19] shadow-xl shadow-slate-900 rounded-2xl">
-      <img className="p-4" src={bannerImg} alt="" />
-      <h3 className="px-4 text-xl md:text-2xl font-bold leading-normal">
-        {title}
-      </h3>
+      <img className="p-4" src={bannerImg} alt={`${title} project banner`} />
+      <h3 className="px-4 text-xl md:text-2xl font-bold leading-normal">{title}</h3>
       <p className="px-4 text-sm md:text-md leading-tight py-2">{main}</p>
       <div className="mt-2 p-2 md:p-4 flex gap-2 md:gap-4">
-        <button className="md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]">
+        <a
+          href={demoLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697] text-center"
+        >
           Demo
-        </button>
-        <button className="md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]">
+        </a>
+        <a
+          href={sourceLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697] text-center"
+        >
           Source Code
-        </button>
+        </a>
       </div>
     </div>
   );
