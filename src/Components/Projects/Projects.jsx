4bccd3bd0dd75fd9c67b056832ede70aace44245
TSdebug <taworkspace01@gmail.com>
2025-09-16 02:43:23 +0530

    Updated projects

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -7,20 +7,20 @@ const Projects = () => {
       <h1 className="text-2xl md:text-4xl text-white font-bold">Projects</h1>
       <div className="py-12 px-8 flex flex-wrap gap-5">
         <ProjectCard
-          title="Blogging Website"
-          main="this is a bloggin website created in next js and used some component library used some component library"
+          title="Bg-Color Changer"
+          main="A background color changing mini project using React Vite, making use of the basics of React features."
         />
         <ProjectCard
-          title="Youtue Clone"
-          main="this is a bloggin website created in next js and used some component library used some component library"
+          title="To-Do-List"
+          main="A theme changing mini project using JavaScript, to show how the Javascript is used to add logic."
         />
         <ProjectCard
-          title="Netflix Clone"
-          main="this is a blogging website created this in next js and used some component library used some component library"
+          title="Code Generator"
+          main="A React vite and tailwindcss project to show how react hooks work and the logics used."
         />
         <ProjectCard
-          title="Netflix Clone"
-          main="this is a blogging website created this in next js and used some component library used some component library"
+          title="Theme Changer"
+          main="A theme changing mini project using React Vite, just like the toggle button for the offical tailwindcss site"
         />
       </div>
     </div>
